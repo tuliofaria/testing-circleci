ecd99d81deeaaa6effc5a385697b42d56c259c2f
Tulio Faria <tuliofaria@gmail.com>
2019-06-04 16:13:44 -0300

    fix

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,8 @@
 # !/bin/bash
 set -e
 echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
-echo $ACCOUNT_KEY_STAGING > ${HOME}/gcloud-service-key.json
+echo $ACCOUNT_KEY_STAGING > service_key.txt
+base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project $PROJECT_ID
 gcloud --quiet config set container/cluster $CLUSTER_NAME
