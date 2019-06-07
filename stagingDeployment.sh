9c7a683ee072855a93b7ead548b8d3365c5aabaf
Júlio Resende Ribeiro <julioresende@gmail.com>
2019-06-07 16:49:41 -0300

    Update stagingDeployment.sh

--- stagingDeployment.sh
+++ stagingDeployment.sh
@@ -11,9 +11,9 @@ if [ "${CIRCLE_BRANCH}" == "staging" ]; then
 fi 
 
 echo "Deploying ${DOCKER_IMAGE_BASE_NAME} to ${CIRCLE_BRANCH}"
-echo $STAGING_ACCOUNT_KEY > service_key.txt
+echo $ACCOUNT_KEY > service_key.txt
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account ${STAGING_ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project e2e-staging-242915
 gcloud --quiet config set container/cluster $PROJECT_NAME-$KUBERNETES_CLUSTER_NAME_SUFFIX
 gcloud config set compute/zone $ZONE
