4dc60cc504d46b4b4e89c1e2b9f7f54afaaf186e
Júlio Resende Ribeiro <julioresende@gmail.com>
2019-06-07 16:16:32 -0300

    Update setupStaging.sh

--- setupStaging.sh
+++ setupStaging.sh
@@ -1,4 +1,6 @@
 # !/bin/bash
 set -e
-export PROJECT_ID=e2e-staging-242915	
+export PROJECT_ID=e2e-staging-242915
 export PROJECT_NAME=e2e-staging
+echo "SET PROJECT_ID=${PROJECT_ID}"
+echo "SET PROJECT_NAME=${PROJECT_NAME}"
