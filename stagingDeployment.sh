1f5cc05551825f3230a58532ce94ca5b047421e3
Júlio Resende Ribeiro <julioresende@gmail.com>
2019-06-07 16:41:35 -0300

    Update stagingDeployment.sh

--- stagingDeployment.sh
+++ stagingDeployment.sh
@@ -1,9 +1,19 @@
 # !/bin/bash
 set -e
+export ZONE=us-central1-a
+export DOCKER_IMAGE_BASE_NAME=appweb
+export KUBERNETES_CLUSTER_NAME_SUFFIX=kb-cluster
+if [ "${CIRCLE_BRANCH}" == "staging" ]; then
+  export PROJECT_ID=e2e-staging-242915	
+  export PROJECT_NAME=e2e-staging
+  export ACCOUNT_ID=${STAGING_ACCOUNT_ID}
+  export ACCOUNT_KEY=${STAGING_ACCOUNT_KEY}
+fi 
+
 echo "Deploying ${DOCKER_IMAGE_BASE_NAME} to ${CIRCLE_BRANCH}"
-echo ${STAGING_ACCOUNT_KEY} > service_key.txt
+echo ${ACCOUNT_KEY} > service_key.txt
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account ${STAGING_ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project $PROJECT_ID
 gcloud --quiet config set container/cluster ${PROJECT_NAME}-${KUBERNETES_CLUSTER_NAME_SUFFIX}
 gcloud config set compute/zone $ZONE
