81aae4bc652202955e8ef545ecce2171f51fdb88
Júlio Resende Ribeiro <julioresende@gmail.com>
2019-06-07 16:20:39 -0300

    Update stagingDeployment.sh

--- stagingDeployment.sh
+++ stagingDeployment.sh
@@ -4,9 +4,9 @@ echo "Deploying ${DOCKER_IMAGE_BASE_NAME} to ${CIRCLE_BRANCH}"
 echo ${STAGING_ACCOUNT_KEY} > service_key.txt
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account ${STAGING_ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
-gcloud config set project $PROJECT_ID
+gcloud config set project ${PROJECT_ID}
 gcloud --quiet config set container/cluster ${PROJECT_NAME}-${KUBERNETES_CLUSTER_NAME_SUFFIX}
-gcloud config set compute/zone $ZONE
+gcloud config set compute/zone ${ZONE}
 gcloud --quiet container clusters get-credentials ${PROJECT_NAME}-${KUBERNETES_CLUSTER_NAME_SUFFIX}
 docker build -t gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_BASE_NAME}:$CIRCLE_SHA1 .
 gcloud docker -- push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_BASE_NAME}:$CIRCLE_SHA1
